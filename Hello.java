84e8920ce758ca258b5c0794b2a19854ebfc2934
olga-kacala <olga.kacala@gmail.com>
2023-10-26 16:13:07 +0200

    Project settings

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Olga";
+        int age = 34;
+
+        System.out.println("Hello " + name + "!" + " You are " + age + " years old ;(");
+
     }
 }
